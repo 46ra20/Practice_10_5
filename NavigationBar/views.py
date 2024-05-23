fe54b03cda1bf02eef4259824243a23db997f9a5
46ra20 <rakib2046.md@gmail.com>
2024-05-23 15:21:43 +0600

    templates update

--- NavigationBar/views.py
+++ NavigationBar/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def Test(request):
-    data={'name':'rakib','salary':'10000'}
+    data={'name':'mD Rakib Bhuiyan','salary':'10000'}
     return render(request,'index.html',data)
